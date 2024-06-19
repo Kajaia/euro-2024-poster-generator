bd720dc77cbd394a5bcde7c9db52684b10047fb8
Kajaia <lashakajaia31@gmail.com>
2024-06-19 18:21:42 +0400

    Add flask with api endpoint

--- match_data.py
+++ match_data.py
@@ -17,6 +17,6 @@ def get_match_data(match_id):
         home_logo_url = match['teams']['home']['img']
         away_logo_url = match['teams']['away']['img']
 
-        generate_match_image(home_team, away_team, home_logo_url, away_logo_url)
+        return generate_match_image(home_team, away_team, home_logo_url, away_logo_url)
     except Exception as e:
         return f'An unexpected error occurred: {e}'
