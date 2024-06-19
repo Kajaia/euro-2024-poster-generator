e800302c5baa3c83600ed2dcbdd49b3ee848071f
Kajaia <lashakajaia31@gmail.com>
2024-06-19 21:55:48 +0400

    Switch to package

--- generate.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-match_id = input('Match ID: ')
-response = requests.post('http://127.0.0.1:5000/generate_poster', json={'match_id': match_id})
-
-if response.status_code == 200:
-    print('Poster generated successfully:', response.json()['result'])
-else:
-    print('Error:', response.json()['error'])
--- image_download.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import requests
-from io import BytesIO
-
-def download_image(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    return Image.open(BytesIO(response.content))
